120f224a76c12f37e32ae602dda1e151a5a5eeee
aryankuag <alemantrixaryan@gmail.com>
2022-10-11 14:02:40 +0530

    a lot of things done

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:admin@cluster0.j3jgmrf.mongodb.net/?retryWrites=true&w=majority');
 const cors = require('cors')
 require('dotenv').config()
-
+app.use(express.urlencoded());
 app.use(cors())
+app.use(express.json())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// mongoose model
+const User = mongoose.model('User', { name: String });
+
 app.post('/api/users', (req, res) => {
-  res.send('')
+  const username = req.body.username
+  res.send(username)
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
